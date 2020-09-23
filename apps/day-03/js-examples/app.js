d22a3534da3d5c20594fc012da56ea7539b3844e
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-09-23 19:01:28 +0530

    commit code at the start of day 3

---
+++ apps/day-03/js-examples/app.js
@@ -0,0 +1,15 @@
+console.log('JS Demos');
+// int, float, char
+var a = 10;
+// ...
+// ...
+// ...
+// TypeScript
+a = 20;
+var b = 100;
+var result = b / a;
+console.log('result:', result);
+// C#
+// Java
+// C++
+// OOPs - Interface
---
+++ apps/day-03/js-examples/app.ts
@@ -0,0 +1,24 @@
+console.log('JS Demos');
+
+// int, float, char
+
+var a: number = 10;
+
+// ...
+// ...
+// ...
+
+// TypeScript
+
+a = 20;
+
+var b = 100;
+var result = b / a;
+
+console.log('result:', result);
+
+// C#
+// Java
+// C++
+
+// OOPs - Interface
