f3a7ecc666371d57376fd25061f33a7b9eda89cb
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-09-24 18:59:19 +0530

    add first app and helpers into the repo

---
+++ apps/helpers/data/data.ts
@@ -0,0 +1,38 @@
+export const users = [
+  { id: 1, name: 'ram' },
+  { id: 2, name: 'hari' },
+  { id: 3, name: 'krish' }
+];
+
+export const posts = [
+  { id: 1, userId: 1, title: 'ram-p1' },
+  { id: 2, userId: 1, title: 'ram-p2' },
+  { id: 3, userId: 2, title: 'hari-p1' },
+  { id: 4, userId: 2, title: 'hari-p2' },
+  { id: 5, userId: 3, title: 'krish-p1' },
+  { id: 6, userId: 3, title: 'krish-p2' }
+];
+
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone X',
+    description: 'A mobile phone from Apple',
+    isAvailable: true,
+    price: 60000
+  },
+  {
+    id: 2,
+    name: 'Samsung Galaxy Note 10',
+    description: 'A mobile phone from Samsung',
+    isAvailable: true,
+    price: 80000
+  },
+  {
+    id: 3,
+    name: 'Google Pixel 3',
+    description: 'A mobile phone from Google',
+    isAvailable: false,
+    price: 50000
+  }
+];
