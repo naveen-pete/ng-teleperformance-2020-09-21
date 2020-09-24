e37fb18ccb0bb3af3f858f5c1017a92c27e0d02f
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-09-24 20:21:34 +0530

    commit binding code into the repo

--- apps/day-04/my-first-app/src/app/products/products.component.ts
+++ apps/day-04/my-first-app/src/app/products/products.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  productId: number = 10;
+  productName: string = 'Samsung Galaxy Note 10'; // M-V- ViewModel
+  productPrice: number = 50000;
 
-  constructor() { }
+  constructor() {
+    // this.productId = 10;
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('Product saved...');
+  }
+
+  onSearch(e) {
+    console.log('Searching...', e.target.value);
   }
 
 }
