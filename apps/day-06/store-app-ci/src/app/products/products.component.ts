550028f5ff376dc572102259718c30edcadbc251
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-09-28 13:49:51 +0530

    commit code for day 6

---
+++ apps/day-06/store-app-ci/src/app/products/products.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ProductModel } from './product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products: ProductModel[] = [
+    {
+      id: 1,
+      name: 'iPhone X',
+      description: 'A mobile phone from Apple',
+      isAvailable: true,
+      price: 60000
+    },
+    {
+      id: 2,
+      name: 'Samsung Galaxy Note 10',
+      description: 'A mobile phone from Samsung',
+      isAvailable: true,
+      price: 80000
+    },
+    {
+      id: 3,
+      name: 'Google Pixel 3',
+      description: 'A mobile phone from Google',
+      isAvailable: false,
+      price: 50000
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onProductCreated(newProduct: ProductModel) {
+    this.products.unshift(newProduct);
+  }
+
+}
